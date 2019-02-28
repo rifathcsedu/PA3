6ecfaa335ca702259c1ba6e28018e52002caa9a7
Saidur <sr.rifat@gmail.com>
2019-02-28 12:25:38 -0700

    MessageDelay file added

--- connection_2CSQS.py
+++ connection_2CSQS.py
@@ -1,45 +1,37 @@
-import threading
-import time
-import botocore
-import os.path
 import csv
-import boto3
-import thread
-import socket
-import random
-def Receive(region,url):
-        while(True):
-            try:
-                sqs = boto3.client('sqs',region_name=region)        
-                queue_url = url
-                response = sqs.receive_message(
-                   QueueUrl=queue_url,
-                   AttributeNames=[
-                            'SentTimestamp'
-                    ],
-                MaxNumberOfMessages=1,
-                MessageAttributeNames=[
-                            'All'
-                        ],
-                VisibilityTimeout=0,
-                WaitTimeSeconds=0
-                    )
-                
-                if(response['ResponseMetadata']['HTTPHeaders']['content-length']!='240'):
-                    #print response        
-                    message = response['Messages'][0]
-                    receipt_handle = message['ReceiptHandle']    
-                    sqs.delete_message(
-                            QueueUrl=queue_url,
-                            ReceiptHandle=receipt_handle
-                        )
-                else:
-                	  
-                    break
-                        
-            except ValueError as e:
-                print("Value error")
+import re
+import sys
+import os
+import subprocess
+import time
+import threading
+from subprocess import Popen
+NTest=1
+timelist=[]
+for i in range(NTest):
+    cmd="sudo tshark -i eth0 -w /tmp/test.pcap -F libpcap &"
+    Popen(cmd, shell=True)
+    time.sleep(3)
+    cmd="python SendMessage.py &"
+    Popen(cmd, shell=True)
+    time.sleep(10)
+    cmd="sudo killall tshark"
+    Popen(cmd, shell=True)
+    time.sleep(2)
+    cmd="sudo tshark -r /tmp/test.pcap -Y 'tcp.flags == 0x010 && ip.addr==3.82.28.86'" # tracing SYN
+    result=subprocess.check_output(cmd, shell=True)
+    result=result.split()
+    end=result[1]
+    print(result)
+    cmd="sudo tshark -r /tmp/test.pcap -Y 'tcp.flags == 0x002 && ip.addr==3.82.28.86'" # tracing ACK
+    result1=subprocess.check_output(cmd, shell=True)
+    result1=result1.split()
+    start=result1[1]
+    timelist.append((end-start)*1000)
 
-url='https://sqs.us-east-1.amazonaws.com/621120329648/ServerQueue'
-region='us-east-1'                
-Receive(region,url)
+mini=min(timelist)
+maxi=max(timelist)
+avg=sum(timelist)*1.0/len(timelist)
+print(mini)
+print(maxi)
+print(avg)
