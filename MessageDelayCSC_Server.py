dd5e97b0db37a814e26c02bbe4d2f748065a350c
Ubuntu <ubuntu@ip-172-31-85-85.ec2.internal>
2019-03-03 23:11:47 +0000

    Server added

--- MessageDelayCSC_Server.py
+++ MessageDelayCSC_Server.py
@@ -1,16 +1,20 @@
+import calendar
+#import pytz
 import csv
 import re
 import sys
 import os
 import subprocess
 import time
+import datetime;
 import threading
 from subprocess import Popen
-NTest=1
+NTest=10
 timelist=[]
 for i in range(NTest):
 	 #time.sleep(10)  
-    start=time.time()
+    start=datetime.datetime.utcnow().timestamp()
+    #start=calendar.timegm(start.utctimetuple())
     print(start)
     cmd="python ReceiveMessage.py S"
     result=subprocess.check_output(cmd, shell=True)
@@ -18,6 +22,7 @@ for i in range(NTest):
     cmd="python SendMessage.py 2"
     result=subprocess.check_output(cmd, shell=True)
     print(result)
+    time.sleep(10)
     timelist.append((start))
     
 print(timelist)
